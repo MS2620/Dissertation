53c3dc2aefb4e63c740c55ad7373dd81a8d810be
MS2620 <113364931+MS2620@users.noreply.github.com>
2025-02-08 12:36:10 +0000

    Update OAuth sign-up functions to use dynamic origin for redirect URLs

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,16 +1,19 @@
 "use server";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
+  const origin = headers().get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/api/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
@@ -19,10 +22,12 @@ export async function signUpWithGithub() {
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
+  const origin = headers().get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/api/oauth`,
+    `${origin}/sign-up`
   );
 
   return redirect(redirectUrl);
